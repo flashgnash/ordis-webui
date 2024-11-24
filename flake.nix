37d20aabb290cd27a9bc61976c7eff72c0fc12ba
flashgnash <flashgnash@proton.me>
2024-11-24 20:05:40 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Ordis discord bot";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ bun ];
+
+        };
+
+      }
+    );
+}
