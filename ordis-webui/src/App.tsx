e80c4f4bd0b2b99ff8815660a6422f5ee65b2514
flashgnash <flashgnash@proton.me>
2025-02-21 19:44:50 +0000

    Update globe to latest version from mockup

--- ordis-webui/src/App.tsx
+++ ordis-webui/src/App.tsx
@@ -2,40 +2,45 @@ import "./App.css";
 import React from "react";
 
  
-function Globe({value, max, regen = null}) {
+function Globe({name, value, max, col="red", regen = null}) {
   return (
-    <div className="card w-100 m-10">
-      <div className="card-header">Hunger</div>
 
-      <div className="card-body">
-        <progress
-          className="orb orb-yellow m-bottom-10"
-          value={value}
-          max={max}
-        ></progress>
 
-        <div className="flex-row m-auto">
-          <div className="m-auto">{value}</div>
-          <div className="m-auto">|</div>
+      <div className="card flex-grow-5 p-10 m-10 mw-25">
+        <div className="card-header">{name}</div>
 
-          <div className="m-auto">{max}</div>
-        </div>
+        <div className="card-body">
+        <div className="orb m-auto">
+          <progress
+            className={`orb-${col} m-bottom-10`}
+            value={value}
+            max={max}
+          ></progress>
+          </div>
+
+          <div className="flex-row m-auto">
+            <div className="m-auto">{value}</div>
+            <div className="m-auto">|</div>
+
+            <div className="m-auto">{max}</div>
+          </div>
 
           {regen != null && 
 
             <div className="m-bottom-10">♻️{regen}</div>
           }
 
-                
+        </div>
       </div>
-    </div>
+
+
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <Globe value="5" max="10" />
+      <Globe name="Armour" value="5" max="10" col="yellow" />
     </div>
   );
 }
