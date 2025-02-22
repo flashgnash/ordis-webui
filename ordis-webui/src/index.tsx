e05342ae0c08e9936c97edee7c833ee0ebea6fc0
flashgnash <flashgnash@proton.me>
2025-02-22 23:03:00 +0000

    Move rolls to its own page, add sidebar for navigation

--- ordis-webui/src/index.tsx
+++ ordis-webui/src/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App.tsx';
+
+
+import CharacterSheet from "./pages/character_sheet.tsx"
+import RollPanel from "./pages/rolls.tsx"
+
+
 import reportWebVitals from './reportWebVitals';
+import { FlexRow } from "./components/common.tsx"
+import Sidenav from "./components/sidenav.tsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <Router>
+    <FlexRow>
+      <Sidenav />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/character_sheet" element={<CharacterSheet />} />
+        <Route path="/rolls" element={
+
+          <RollPanel />
+
+        } />
+
+      </Routes>
+    </FlexRow>
+  </Router>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
