4403429f087971d12220f5d8cba4c426326348c9
flashgnash <flashgnash@proton.me>
2025-08-23 13:51:54 +0100

    Spell was not using the interfaces & struct

--- dotnet/Models/Spell.cs
+++ dotnet/Models/Spell.cs
@@ -1,8 +1,10 @@
-public class Spell {
+public struct Spell: IDescriptable, IRollable {
+
 	public string? Icon {get; set;}
+
 	public string Name {get; set;}
+
 	public Dictionary<string,string>? Rolls {get; set;}	
 
-	
 }
 
